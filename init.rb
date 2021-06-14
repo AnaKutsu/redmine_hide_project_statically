dbc56f898de7c52c6b81e8c5e57eca79572f233e
AnaKutsu <ana.kutsu@gmail.com>
2021-06-15 02:16:54 +0900

    Update plugin descriptions.

--- init.rb
+++ init.rb
@@ -1,8 +1,10 @@
 require_dependency 'redmine_hide_project_statically/patch'
 
 Redmine::Plugin.register :redmine_hide_project_statically do
-  name 'Force hide project statically'
+  name 'Force hide projects statically'
   author 'Takashi Ishiakwa'
-  description ''
+  description 'Hide redmine projects from non-defined users (even if user has admin permission).'
   version '0.0.1'
+  url 'https://github.com/AnaKutsu/redmine_hide_project_statically'
+  author_url 'https://www.ana-kutsu.com/'
 end
